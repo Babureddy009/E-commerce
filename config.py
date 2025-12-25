e87dbed3bd4b4033ceb46990646d132ad23900af
Babureddy B N <141361152+Babureddy009@users.noreply.github.com>
2025-12-26 00:45:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/uploads/products')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
 
-SECRET_KEY = "supersecretkey"
-
-
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 
 MYSQL_CONFIG = {
-    "host": "host.docker.internal",
-    "user": "root",
-    "password": "Babu@123",
-    "database": "ecommerce"
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
 }
-
-SECRET_KEY = "supersecretkey"
-
